bcfe381c0d379d3da85cb30e1216848d88f31c33
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2026-09-17 21:00:26 -0300

    Configuração
    chore: configuração inicial do projeto (backend)

---
+++ backend/src/main/java/com/prestify/erp/PrestifyErpApplication.java
@@ -0,0 +1,11 @@
+package com.prestify.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestifyErpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrestifyErpApplication.class, args);
+    }
+}
